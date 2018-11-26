c3b47b88cc9d66cdb53f05e38d248e739554c41d
syntaxkim <lovems0219@gmail.com>
2018-11-26 18:01:31 +0900

    admin.py updated

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from orders.models import Category, Pizza, Topping, Sub, Pasta, Salad, Dinner
 
 # Register your models here.
+admin.site.register(Category)
+admin.site.register(Pizza)
+admin.site.register(Topping)
+admin.site.register(Sub)
+admin.site.register(Pasta)
+admin.site.register(Salad)
+admin.site.register(Dinner)
