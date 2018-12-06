77acb29589f81c9e538f4f5cdf1d8095c090f463
syntaxkim <lovems0219@gmail.com>
2018-12-06 17:21:56 +0900

    orders/admin.py & reviwes/admin.py updated

--- orders/admin.py
+++ orders/admin.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
+
 from orders.models import Category, Pizza, Topping, Sub, Extra, Pasta, Salad, Dinner, CartItem, OrderItem, Order
 
 # Register your models here.
 
 class OrderAdmin(admin.ModelAdmin):
     filter_horizontal = ("items",)
+    list_display = ('order_time', 'user', 'contact', 'subtotal', 'recieved')
+    list_filter = ['order_time', 'status', 'recieved']
+    search_fields = ['user', 'contact', 'message', 'billing_address', 'shipping_address']
 
 class ToppingAdmin(admin.ModelAdmin):
     filter_horizontal = ("toppings",)
--- reviews/admin.py
+++ reviews/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
-from reviews.models import Review
+
+from reviews.models import Review, User
 
 # Register your models here.
-admin.site.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('title', 'user', 'time_created')
+    list_filter = ['time_created']
+    search_fields = ['user', 'title', 'content']
+
+admin.site.register(Review, ReviewAdmin)
