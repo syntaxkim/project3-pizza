e6208bbff04e0614c97e9660de8a587921125de3
syntaxkim <lovems0219@gmail.com>
2018-12-06 22:29:47 +0900

    issue of price calculation in item page fixed

--- orders/static/orders/item_detail.js
+++ orders/static/orders/item_detail.js
@@ -1,21 +1,40 @@
 document.addEventListener('DOMContentLoaded', () => {
-    // Subtotal
-    item_price = document.querySelector('#price').innerHTML;
+    get_price();
+});
 
-    quantity = 1;
+// Get overall price of item and options
+function get_price() {
+    // Get initial price of item.
+    var item_price = parseInt(document.getElementById('price').innerHTML);
+    var quantity = 1;
+    console.log(item_price);
+
+    // Get extra price if it exists.
+    if (!(document.getElementById('extra_price') === null)) {
+        var extra_price = parseInt(document.getElementById('extra_price').innerHTML);
+        console.log(extra_price);
+    };
+
+    // When quantity changed,
     document.querySelector('#quantity').onchange = function() {
         quantity = this.value;
         document.querySelector('#price').innerHTML = item_price * quantity;
+        console.log(`Quantity changed to ${quantity}.`);
     };
-
-    extra.onchange = function() {
-        extra_price = document.querySelector('#extra_price').innerHTML;
-        price = parseInt(document.querySelector('#price').innerHTML);
-        extra_price = parseInt(quantity) * parseInt(extra_price);
-        if (extra.checked) {
-            document.querySelector('#price').innerHTML = price + extra_price;
-        } else {
-            document.querySelector('#price').innerHTML = price - extra_price;
-        }
+    
+    // If extra exists,
+    if (!(document.getElementById('extra') === null)) {
+        // Whenever checkbox checked,
+        extra.onchange = function() {
+            if (extra.checked) {
+                item_price += extra_price;
+                document.querySelector('#price').innerHTML = item_price * quantity;
+                console.log(`Checked. Price + (${extra_price} * ${quantity})`);
+            } else {
+                item_price -= extra_price;
+                document.querySelector('#price').innerHTML = item_price * quantity;
+                console.log(`Unhecked. Price - (${extra_price} * ${quantity})`);
+            };
+        };
     };
-});
+};
